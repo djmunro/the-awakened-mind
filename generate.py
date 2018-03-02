26119d6f1b7ce6cc51b4d1cecb5835d1a75cdee5
David Munro <david.munro@nokia.com>
2018-03-02 16:32:51 -0500

    implementing dict for filename: content

--- generate.py
+++ generate.py
@@ -1,21 +1,3 @@
-'''
-Step 1 - Read
-Listing files in a directory, reading files and writing files are most
-likely part of the standard library of any high level language - at least I am not aware of one
-that does not provide those functions, please correct me if I am wrong.
-
-
-This step is actually pretty simple - read all files in an directory,
-read the file and add it to the SITE directory with the filename, which we will later use as URL,
-and its content as value.
-
-Lines: 19 to 23
-
-Do It Yourself
-Right now you have to make sure the filename is URL friendly.
-Add a function which takes a string as input and returns an URL friendly version of it.
-Replacing spaces with dashes and cut of file extensions, for example.
-'''
 import os
 
 
@@ -25,12 +7,23 @@ def get_all_file_paths(directory):
 
 
 def read_all_posts(directory):
-    files = get_all_file_paths(directory)
-    content = read_file_content(file)
-    return content
+    return {clean_filename(file): read_file_content(file) for file in get_all_file_paths(directory)}
 
 
-def read_file_content(file):
-    with open(file, u'r') as file:
-        text = file.read()
-    return text
+def clean_filename(filepath):
+    filename = remove_file_path_deatils(filepath)
+    return convert_spaces_to_hyphens(filename)
+
+
+def convert_spaces_to_hyphens(filename):
+    return filename.replace(" ", "-")
+
+
+def remove_file_path_deatils(filepath):
+    return os.path.splitext(os.path.basename(filepath))[0]
+
+
+def read_file_content(filepath):
+    path = os.path.join(os.path.dirname(__file__), filepath)
+    with open(path, 'r') as file:
+        return file.read()
--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -1,14 +1,23 @@
 import unittest
 
-from generate import read_all_posts, get_all_file_paths
+from generate import read_all_posts, get_all_file_paths, clean_filename
 
 
 class GenerateTests(unittest.TestCase):
 
     def test_read_all_posts(self):
         posts = read_all_posts(directory='posts')
-        self.assertTrue(len(posts) >= 1)
+        self.assertEqual({'test-post':'Name of the post'}, posts)
 
     def test_get_all_file_paths_are_valid(self):
         file_paths = get_all_file_paths(directory='posts')
-        self.assertEqual(file_paths[0], 'posts/test-post.md')
+        self.assertEqual('posts/test-post.md', file_paths[0])
+
+    def test_clean_filename(self):
+        cleaned_name = clean_filename('posts/test-post.md')
+        self.assertEqual('test-post', cleaned_name)
+
+    def test_clean_filename_when_file_has_spaces(self):
+        cleaned_name = clean_filename('posts/test post.md')
+        self.assertEqual('test-post', cleaned_name)
+
