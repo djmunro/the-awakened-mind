4b196ce49b880c72d81ca0494a3f37d7a0431d5b
David Munro <david.munro@nokia.com>
2018-03-03 10:17:37 -0500

    cleaning SITE directory

--- generate.py
+++ generate.py
@@ -1,13 +1,35 @@
 import os
 
+SITE_DIR = 'SITE'
 
-def get_all_file_paths(directory):
-    file_names = os.listdir(os.path.join(os.path.dirname(__file__), directory))
-    return [os.path.join(directory, file) for file in file_names]
+
+def deploy_site(posts):
+    clean_site_directory(os.path.join(os.path.dirname(__file__), SITE_DIR))
+    pages = build_post_pages(posts)
+    write_pages(pages, directory=SITE_DIR)
+
+
+def clean_site_directory(directory_path):
+    files = os.listdir(directory_path)
+    for file in files:
+        os.remove('{path}/{file}'.format(path=directory_path, file=file))
+
+
+def build_post_pages(posts):
+    pass
+
+
+def write_pages(pages, directory):
+    pass
 
 
 def read_all_posts(directory):
     return {clean_filename(file): read_file_content(file) for file in get_all_file_paths(directory)}
+
+
+def get_all_file_paths(directory):
+    file_names = os.listdir(os.path.join(os.path.dirname(__file__), directory))
+    return [os.path.join(directory, file) for file in file_names]
 
 
 def clean_filename(filepath):
@@ -15,15 +37,15 @@ def clean_filename(filepath):
     return convert_spaces_to_hyphens(filename)
 
 
-def convert_spaces_to_hyphens(filename):
-    return filename.replace(" ", "-")
+def read_file_content(filepath):
+    path = os.path.join(os.path.dirname(__file__), filepath)
+    with open(path, 'r') as file:
+        return file.read()
 
 
 def remove_file_path_deatils(filepath):
     return os.path.splitext(os.path.basename(filepath))[0]
 
 
-def read_file_content(filepath):
-    path = os.path.join(os.path.dirname(__file__), filepath)
-    with open(path, 'r') as file:
-        return file.read()
+def convert_spaces_to_hyphens(filename):
+    return filename.replace(" ", "-")
--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -1,6 +1,10 @@
+import os
 import unittest
 
-from generate import read_all_posts, get_all_file_paths, clean_filename
+import markdown2
+from jinja2 import Environment
+
+from generate import read_all_posts, get_all_file_paths, clean_filename, clean_site_directory, SITE_DIR
 
 
 class GenerateTests(unittest.TestCase):
@@ -21,3 +25,33 @@ class GenerateTests(unittest.TestCase):
         cleaned_name = clean_filename('posts/test post.md')
         self.assertEqual('test-post', cleaned_name)
 
+    def test_delete(self):
+        # TODO temp dir
+        parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        open("{}/{}/test.file".format(parent_dir, SITE_DIR), "w+")
+        clean_site_directory(os.path.join(parent_dir, SITE_DIR))
+
+    def test_rendering_template(self):
+        template = '''
+        <!DOCTYPE html>
+        <html>
+            <body>
+                {{content}}
+            </body>
+        </html>
+        '''
+        expected_html = '''
+        <!DOCTYPE html>
+        <html>
+            <body>
+                Test content
+            </body>
+        </html>
+        '''
+        html = Environment().from_string(template).render(content='Test content')
+        self.assertEqual(expected_html, html)
+
+    def test_markdown_converter(self):
+        html_from_md = markdown2.markdown("*boo!*")
+        expected_html = '<p><em>boo!</em></p>\n'
+        self.assertEqual(expected_html, html_from_md)
